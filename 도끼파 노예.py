27daa5e45c0e06ad9721bb011af8860348cb5bf5
sobong13 <39268717+sobong13@users.noreply.github.com>
2019-08-13 12:02:32 +0900

    Add files via upload

---
+++ 도끼파 노예.py
@@ -0,0 +1,36 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("!안녕")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!안녕"):
+        await message.channel.send("이게 어른한테 어디서 반말이야!")
+
+    if message.content.startswith("!도움말"):
+        await message.channel.send("그런거 없는디?")
+
+    if message.content.startswith("!사진"):
+        pic = message.content.split(" ")[1]
+        await message.channel.send(file=discord.File(pic))
+
+    if message.content.startswith("굿"):
+        await message.channel.send("고맙데이~")
+
+    if message.content.startswith("/채널메시지"):
+        channel = message.content[7:25]
+        msg = message.content[26:]
+        await client.get_channel(int(channel)).send(msg)
+
+
+client.run("NjEwNDY2MTg3OTAxNTM0MjA4.XVFr4Q.gBDcI9mBgD9veJeBlFhOWBECxnE")
